59a80a289e8021b0703baf3c5e9bafb979762be2
MomDra <yem9087@koreatech.ac.kr>
2021-03-27 23:11:21 +0900

    Bird Animation

--- Assets/My/Scipts/MyBird.cs
+++ Assets/My/Scipts/MyBird.cs
@@ -9,10 +9,12 @@ public class MyBird : MonoBehaviour
 
     bool isDead = false;
     Rigidbody2D myRigid;
+    Animator myAmimator;
 
     void Awake()
     {
         myRigid = GetComponent<Rigidbody2D>();
+        myAmimator = GetComponent<Animator>();
     }
 
     void Update()
@@ -23,6 +25,7 @@ public class MyBird : MonoBehaviour
             {
                 myRigid.velocity = Vector2.zero;
                 myRigid.AddForce(new Vector2(0, upForce));
+                myAmimator.SetTrigger("Flap");
             }
         }
     }
@@ -30,5 +33,6 @@ public class MyBird : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision)
     {
         isDead = true;
+        myAmimator.SetTrigger("Die");
     }
 }
